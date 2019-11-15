4944b5bee2bf4d92c5db7b688204be562d8fc46a
Volodymyr Zavorotiuk <Volodymyr_Zavorotiuk@epam.com>
2019-11-15 13:13:32 +0200

    Update constants file

--- src/constants.ts
+++ src/constants.ts
@@ -2,9 +2,8 @@ const CONSTANTS =  {
     FILE_KEY: 'archive.zip',
     FIRST_BUCKET_NAME: 'test-task-bucket-1',
     SECOND_BUCKET_NAME: 'test-task-bucket-2',
-    THIRD_BUCKET_NAME: 'test-task-bucket-2',
+    THIRD_BUCKET_NAME: 'test-task-bucket-3',
     TEMP_FOLDER_PATH: './_temp'
-    
 }
 
 export default CONSTANTS;
